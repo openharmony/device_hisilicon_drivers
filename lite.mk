12a9947c38ed365cae76d77a87739f00ed7fc5f2
lzl <sucer_fater@163.com>
2021-06-11 08:20:54 +0000

    delete mmc unused head files
    Signed-off-by: lzl <sucer_fater@163.com>

--- lite.mk
+++ lite.mk
@@ -117,14 +117,7 @@ endif
 
 # mmc dirvers
 ifeq ($(LOSCFG_DRIVERS_MMC), y)
-    MMC_HOST_DIR := himci
     LITEOS_BASELIB  += -lmmc
-ifeq ($(BUILD_FROM_SOURCE), y)
-    LIB_SUBDIRS        += $(HISILICON_DRIVERS_SOURCE_ROOT)/mmc
-    LITEOS_MMC_INCLUDE += -I $(HISILICON_DRIVERS_SOURCE_ROOT)/mmc/include
-else
-    LITEOS_MMC_INCLUDE += -I $(HISILICON_DRIVERS_ROOT)/include/mmc/include
-endif
 endif
 
 # mtd drivers
