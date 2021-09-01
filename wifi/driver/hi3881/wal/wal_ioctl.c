777e25b2a325a9f8ecd6cc16ad02ad5763c98403
YOUR_NAME <fangdong7@huawei.com>
2021-09-01 01:47:05 +0000

    Description: p2p解决手机侧主动连接单板失败 Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: YOUR_NAME <fangdong7@huawei.com>

--- wifi/driver/hi3881/wal/wal_ioctl.c
+++ wifi/driver/hi3881/wal/wal_ioctl.c
@@ -3037,7 +3037,7 @@ hi_u32 wal_priv_cmd(oal_net_device_stru *netdev, oal_ifreq_stru *ifr, hi_s32 cmd
     ret = oal_copy_from_user(puc_command, (hi_u32)(priv_cmd.l_total_len), ((hi_u8 *)ifr->ifr_data) + 8, /* 数据偏移量8 */
         (hi_u32)(priv_cmd.l_total_len));
 #endif
-    if (ret != 0) {
+    if (ret != HI_SUCCESS) {
         oam_error_log1(0, 0, "{wal_priv_cmd::oal_copy_from_user: err code [%u] }", ret);
         oal_free(puc_command);
         return ret;
