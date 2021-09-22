d2e78315ccbb9086e9efdfc7503db50bf91fbdf9
YOUR_NAME <wangyeyu1@huawei.com>
2021-09-22 15:31:43 +0800

    修改说明：1.增加mipi_csi_hi35xx驱动
    Signed-off-by: YOUR_NAME <wangyeyu1@huawei.com>

---
+++ mipi_csi/mipi_csi_hi35xx.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 HiSilicon (Shanghai) Technologies CO., LIMITED.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MIPI_CSI_HI35XX_H
+#define MIPI_CSI_HI35XX_H
+
+#include "hdf_base.h"
+
+#define CMOS_MAX_DEV_NUM             1
+#define SNS_MAX_CLK_SOURCE_NUM       2
+#define SNS_MAX_RST_SOURCE_NUM       2
+
+#endif /* MIPI_CSI_HI35XX_H */
