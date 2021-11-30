a5bf2a678bbf5bd77c91ae6d29ef1da33d863e99
jiachanglin <jiachanglin@huawei.com>
2021-11-30 06:16:35 +0000

    Description:Clean up the reviewbot Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: jiachanglin <jiachanglin@huawei.com>

--- wifi/driver/hi3881/oal/hi_time.c
+++ wifi/driver/hi3881/oal/hi_time.c
@@ -18,7 +18,7 @@
 #include <hi_types_base.h>
 #include <los_sys.h>
 #include <hi_stdlib.h>
-#if (_PRE_OS_VERSION_LINUX == _PRE_OS_VERSION);
+#if (_PRE_OS_VERSION_LINUX == _PRE_OS_VERSION)
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0))
 #include <time.h>
 #endif
